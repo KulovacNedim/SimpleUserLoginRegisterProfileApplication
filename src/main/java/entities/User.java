c3ae396317cd4cd10e919dd14f6158c6ee89018e
Kulovac Nedim <nedim.kulovac@gmail.com>
2018-09-13 23:54:34 +0200

    Entities added. Landing page set.

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,124 @@
+package entities;
+
+import java.util.Objects;
+
+public class User {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String pasword;
+    private Address address;
+    private Role role;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, String pasword, Address address, Role role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.pasword = pasword;
+        this.address = address;
+        this.role = role;
+    }
+
+    public User(int id, String firstName, String lastName, String email, String pasword, Address address, Role role) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.pasword = pasword;
+        this.address = address;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasword() {
+        return pasword;
+    }
+
+    public void setPasword(String pasword) {
+        this.pasword = pasword;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", pasword='" + pasword + '\'' +
+                ", address=" + address +
+                ", role=" + role +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(pasword, user.pasword) &&
+                Objects.equals(address, user.address) &&
+                Objects.equals(role, user.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, pasword, address, role);
+    }
+}
